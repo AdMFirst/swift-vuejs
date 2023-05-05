551f495986d4c56a9bac7d2eb79428e4a7d61293
Aditya MP <211wadityaw112@gmail.com>
2023-05-05 19:20:06 +0700

    temp commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="about">
+    <h1>This is a home page</h1>
+  </div>
 </template>
+
+<style scoped>
+
+</style>
